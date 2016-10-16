b1383e7266822ea35c3f421cbc118390d274e380
Dan Cobb <cobbdb@gmail.com>
2016-10-15 20:18:19 -0700

    Added Boiler refresh()

---
+++ boiler.php
@@ -0,0 +1,24 @@
+<?php
+
+include_once('./node_modules/eta-php/H.php');
+include_once('./node_modules/phplumbermill/log.php');
+error_reporting(E_ALL & ~E_NOTICE);
+
+class Boiler {
+    public static function refresh($pages = []) {
+        $start = microtime(true);
+
+        foreach ($pages as $dest => $page) {
+            $model = $page['model'] ?: [];
+            $src = $page['src'];
+            $flatfile = H::render($src, $model, true) or die("Missing view $src!");
+            $template = fopen($dest, 'w') or die("Unable to open file $dest!");
+            fwrite($template, $flatfile);
+            fclose($template);
+        }
+
+        $end = microtime(true);
+        $renderTime = round($end - $start, 6);
+        log::end("render completed in $renderTime seconds");
+    }
+}
--- refresh.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-include_once('./node_modules/eta-php/H.php');
-
-
