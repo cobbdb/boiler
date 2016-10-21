080ccc0c4cf0b58743fce7df7ba178f2ac03e543
Dan Cobb <cobbdb@gmail.com>
2016-10-20 22:45:05 -0700

    .

--- boiler.php
+++ boiler.php
@@ -1,17 +1,33 @@
 <?php
 
-include_once('node_modules/eta-php/H.php');
-include_once('node_modules/phplumbermill/log.php');
+$public = $_SERVER['DOCUMENT_ROOT'];
+$modules = "$public/../php_modules";
+include_once("$modules/eta-php/H.php");
+include_once("$modules/phplumbermill/log.php");
 error_reporting(E_ALL & ~E_NOTICE);
 
 class Boiler {
-    public static function refresh($pages = []) {
+    /**
+     * ## refresh(base, pages)
+     * @param {string} base Path to site-wide base template.
+     * @param {array<dest, array<view, [model]>>} pages
+     * @example
+     *  refresh('./base.html', [
+     *      '../public/page.html' => [
+     *          'view' => '../private/view.html',
+     *          'model' => []
+     *      ]
+     *  ]);
+     */
+    public static function refresh($base, $pages = []) {
         $start = microtime(true);
 
         foreach ($pages as $dest => $page) {
             $model = $page['model'] ?: [];
-            $src = $page['src'];
-            $flatfile = H::render($src, $model, true) or die("Missing view $src!");
+            $view = $page['view'];
+            $flatfile = H::render($base, [
+                'body' => H::render($view, $model, true) or die("Missing view $src!")
+            ], true) or die("Missing base template $base!");
             $template = fopen($dest, 'w') or die("Unable to open file $dest!");
             fwrite($template, $flatfile);
             fclose($template);
