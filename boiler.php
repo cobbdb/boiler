df4a169dff063643bdb5bc097ff5866c58813865
Dan Cobb <cobbdb@gmail.com>
2016-10-23 15:24:27 -0700

    .

--- boiler.php
+++ boiler.php
@@ -2,37 +2,49 @@
 
 include_once("$MODULES/eta/H.php");
 include_once("$MODULES/lumbermill/log.php");
-error_reporting(E_ALL & ~E_NOTICE);
 
+// Set the default views directory.
 H::setHome("$VIEWS/", true);
 
 /**
- * ## refresh(base, pages)
+ * ## BOILER(base, pages)
  * @param {string} base Path to site-wide base template.
  * @param {array<dest, array<view, [model]>>} pages
  * @example
- *  refresh('./base.html', [
+ *  BOILER('./base.html', [
  *      '../public/page.html' => [
  *          'view' => '../private/view.html',
  *          'model' => []
  *      ]
  *  ]);
  */
-function BOILER($base, $pages = []) {
-    $start = microtime(true);
-
-    foreach ($pages as $dest => $page) {
-        $model = $page['model'] ?: [];
-        $view = $page['view'];
-        $flatfile = H::render($base, [
-            'body' => H::render($view, $model) or die("Missing view $src!")
-        ]) or die("Missing base template $base!");
-        $template = fopen($dest, 'w') or die("Unable to open file $dest!");
-        fwrite($template, $flatfile);
-        fclose($template);
+class Boiler {
+    public static function render($base, $pages = []) {
+        echo 'Beginning render...';
+        foreach ($pages as $dest => $page) {
+            $model = $page['model'] ?: [];
+            $view = $page['view'];
+            $content = H::render($view, $model) or die("Missing view $src!");
+            echo $content;
+            $flatfile = H::render($base, [
+                'body' => $content
+            ]) or die("Missing base template $base!");
+            $template = fopen($dest, 'w') or die("Unable to open file $dest!");
+            fwrite($template, $flatfile);
+            fclose($template);
+        }
+        echo '... done!';
     }
 
-    $end = microtime(true);
-    $renderTime = round($end - $start, 6);
-    log::end("render completed in $renderTime seconds");
+    public static function copy($files = []) {
+        echo 'Beginning copy...';
+        foreach ($files as $dest => $path) {
+            $dest = fopen($dest, 'w') or die("Unable to open file $dest!");
+            $content = file_get_contents($path);
+            echo $content;
+            fwrite($dest, $content);
+            fclose($dest);
+        }
+        echo '... done!';
+    }
 }
