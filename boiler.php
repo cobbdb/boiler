aa80c40c5d6fc9088394fa5c55077398231c8591
Dan Cobb <cobbdb@gmail.com>
2016-11-05 14:37:19 -0700

    .

--- boiler.php
+++ boiler.php
@@ -6,19 +6,21 @@ include_once("$MODULES/lumbermill/log.php");
 // Set the default views directory.
 H::setHome("$VIEWS/", true);
 
-/**
- * ## BOILER(base, pages)
- * @param {string} base Path to site-wide base template.
- * @param {array<dest, array<view, [model]>>} pages
- * @example
- *  BOILER('./base.html', [
- *      '../public/page.html' => [
- *          'view' => '../private/view.html',
- *          'model' => []
- *      ]
- *  ]);
- */
 class Boiler {
+    /**
+     * ## render(base, pages)
+     * @param {string} base Path to site-wide base template.
+     * @param {array<dest, array<view, [model]>>} pages
+     * @example
+     * ```php
+     *  Boiler::render('./base.html', [
+     *      '../public/page.html' => [
+     *          'view' => '../private/view.html',
+     *          'model' => []
+     *      ]
+     *  ]);
+     * ```
+     */
     public static function render($base, $pages = []) {
         echo 'Beginning render...';
         foreach ($pages as $dest => $page) {
@@ -36,14 +38,31 @@ class Boiler {
         echo '... done!';
     }
 
-    public static function copy($files = []) {
+    /**
+     * ## copy(base, pages)
+     * @param {array<string, string>} patterns Globbing patterns.
+     * @example
+     * ```php
+     *  Boiler::copy([
+     *      "$PUBLIC/assets/js/" => "$ASSETS/dist/js/*",
+     *      "$PUBLIC/assets/css/" => "$ASSETS/dist/css/*",
+     *      "$PUBLIC/assets/img/" => "$ASSETS/dist/img/*",
+     *      "$PUBLIC/assets/fonts/" => "$ASSETS/dist/fonts/*"]
+     *  ]);
+     * ```
+     */
+    public static function copy($patterns = []) {
         echo 'Beginning copy...';
-        foreach ($files as $dest => $path) {
-            $dest = fopen($dest, 'w') or die("Unable to open file $dest!");
-            $content = file_get_contents($path);
-            echo $content;
-            fwrite($dest, $content);
-            fclose($dest);
+        foreach ($patterns as $dest => $src) {
+            $files = glob($src);
+            foreach ($files as $path) {
+                $filename = basename($path);
+                $fout = fopen("$dest$filename", 'w') or die("Unable to open file $dest$filename!");
+                $content = file_get_contents($path);
+                echo $content;
+                fwrite($fout, $content);
+                fclose($fout);
+            }
         }
         echo '... done!';
     }
